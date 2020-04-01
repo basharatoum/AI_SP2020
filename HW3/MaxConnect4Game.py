2b01f61b4e822fa6dba01aa860360ae3c3465c44
basharatoum <atoumbashar@gmail.com>
2020-03-31 20:11:02 -0500

    hw3

---
+++ HW3/MaxConnect4Game.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+
+# Written by Chris Conly based on C++
+# code provided by Vassilis Athitsos
+# Written to be Python 2.4 compatible for omega
+
+from copy import copy
+import random
+import sys
+
+
+class maxConnect4Game:
+    def __init__(self):
+        self.gameBoard = [[0 for i in range(7)] for j in range(6)]
+        self.currentTurn = 1
+        self.Computer = 0
+        self.Person = 1
+        self.depthLimit = 42
+        self.player1Score = 0
+        self.player2Score = 0
+        self.pieceCount = 0
+        self.gameFile = None
+        random.seed()
+
+    # Count the number of pieces already played
+    def checkPieceCount(self):
+        self.pieceCount = sum(1 for row in self.gameBoard for piece in row if piece)
+
+    # Output current game status to console
+    def printGameBoard(self):
+        print ' -----------------'
+        for i in range(6):
+            print ' |',
+            for j in range(7):
+                print('%d' % self.gameBoard[i][j]),
+            print '| '
+        print ' -----------------'
+
+    # Output current game status to file
+    def printGameBoardToFile(self):
+        for row in self.gameBoard:
+            self.gameFile.write(''.join(str(col) for col in row) + '\r\n')
+        self.gameFile.write('%s\r\n' % str(self.currentTurn))
+
+    # Place the current player's piece in the requested column
+    def playPiece(self, column):
+        if not self.gameBoard[0][column]:
+            for i in range(5, -1, -1):
+                if not self.gameBoard[i][column]:
+                    self.gameBoard[i][column] = self.currentTurn
+                    return 1
+                
+
+
+    def removeLastPiece(self,column):
+        for i in range(6): 
+            if self.gameBoard[i][column]!=0:
+                self.gameBoard[i][column]=0
+                return
+
+
+    def MaxFunc(self,depth,Alpha,Beta):
+        if self.pieceCount == 42 or depth > self.depthLimit:
+            return self.countScore(),-1
+        result = -999999
+        rCol =-1
+        for i in range(7):
+            if self.gameBoard[0][i]==0:
+                self.currentTurn = self.Computer
+                self.playPiece(i)
+                v,col = self.MinFunc(depth+1,Alpha,Beta)
+                if v>result:
+                    rCol = i
+                result = max(result,v)
+                self.removeLastPiece(i)
+                if result>=Beta:
+                    self.currentTurn = self.Computer
+                    return result,rCol
+                Alpha = max(Alpha,result)
+
+        self.currentTurn = self.Computer
+        return result,rCol
+
+        
+
+    def MinFunc(self,depth,Alpha,Beta):
+        if self.pieceCount == 42 or depth > self.depthLimit:
+            return self.countScore(),-1
+
+        result = 999999
+        rCol =-1
+        for i in range(7):
+            if self.gameBoard[0][i]==0:
+                self.currentTurn = self.Computer%2+1
+                self.playPiece(i)
+                v,col = self.MaxFunc(depth+1,Alpha,Beta)
+                if v<result:
+                    rCol = i
+                result = min(result,v)
+                self.removeLastPiece(i)
+                if result<=Alpha:
+                    self.currentTurn = self.Computer%2+1
+                    return result,rCol
+                Beta = min(Beta,result)
+        self.currentTurn = self.Computer%2+1
+        return result,rCol
+        
+    # The AI section. Currently plays randomly.
+    def aiPlay(self):
+        result,col = self.MaxFunc(0,-99999,99999)
+        self.playPiece(col)
+        self.pieceCount += 1
+        print('\n\nmove %d: Player %d, column %d\n' % (self.pieceCount, self.currentTurn, col+1))
+        if self.currentTurn == 1:
+            self.currentTurn = 2
+        elif self.currentTurn == 2:
+            self.currentTurn = 1
+    
+
+
+    # Calculate the number of 4-in-a-row each player has
+    def countScore(self):
+        self.player1Score = 0;
+        self.player2Score = 0;
+
+        # Check horizontally
+        for row in self.gameBoard:
+            # Check player 1
+            if row[0:4] == [1]*4:
+                self.player1Score += 1
+            if row[1:5] == [1]*4:
+                self.player1Score += 1
+            if row[2:6] == [1]*4:
+                self.player1Score += 1
+            if row[3:7] == [1]*4:
+                self.player1Score += 1
+            # Check player 2
+            if row[0:4] == [2]*4:
+                self.player2Score += 1
+            if row[1:5] == [2]*4:
+                self.player2Score += 1
+            if row[2:6] == [2]*4:
+                self.player2Score += 1
+            if row[3:7] == [2]*4:
+                self.player2Score += 1
+
+        # Check vertically
+        for j in range(7):
+            # Check player 1
+            if (self.gameBoard[0][j] == 1 and self.gameBoard[1][j] == 1 and
+                   self.gameBoard[2][j] == 1 and self.gameBoard[3][j] == 1):
+                self.player1Score += 1
+            if (self.gameBoard[1][j] == 1 and self.gameBoard[2][j] == 1 and
+                   self.gameBoard[3][j] == 1 and self.gameBoard[4][j] == 1):
+                self.player1Score += 1
+            if (self.gameBoard[2][j] == 1 and self.gameBoard[3][j] == 1 and
+                   self.gameBoard[4][j] == 1 and self.gameBoard[5][j] == 1):
+                self.player1Score += 1
+            # Check player 2
+            if (self.gameBoard[0][j] == 2 and self.gameBoard[1][j] == 2 and
+                   self.gameBoard[2][j] == 2 and self.gameBoard[3][j] == 2):
+                self.player2Score += 1
+            if (self.gameBoard[1][j] == 2 and self.gameBoard[2][j] == 2 and
+                   self.gameBoard[3][j] == 2 and self.gameBoard[4][j] == 2):
+                self.player2Score += 1
+            if (self.gameBoard[2][j] == 2 and self.gameBoard[3][j] == 2 and
+                   self.gameBoard[4][j] == 2 and self.gameBoard[5][j] == 2):
+                self.player2Score += 1
+
+        # Check diagonally
+
+        # Check player 1
+        if (self.gameBoard[2][0] == 1 and self.gameBoard[3][1] == 1 and
+               self.gameBoard[4][2] == 1 and self.gameBoard[5][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][0] == 1 and self.gameBoard[2][1] == 1 and
+               self.gameBoard[3][2] == 1 and self.gameBoard[4][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][1] == 1 and self.gameBoard[3][2] == 1 and
+               self.gameBoard[4][3] == 1 and self.gameBoard[5][4] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][0] == 1 and self.gameBoard[1][1] == 1 and
+               self.gameBoard[2][2] == 1 and self.gameBoard[3][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][1] == 1 and self.gameBoard[2][2] == 1 and
+               self.gameBoard[3][3] == 1 and self.gameBoard[4][4] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][2] == 1 and self.gameBoard[3][3] == 1 and
+               self.gameBoard[4][4] == 1 and self.gameBoard[5][5] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][1] == 1 and self.gameBoard[1][2] == 1 and
+               self.gameBoard[2][3] == 1 and self.gameBoard[3][4] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][2] == 1 and self.gameBoard[2][3] == 1 and
+               self.gameBoard[3][4] == 1 and self.gameBoard[4][5] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][3] == 1 and self.gameBoard[3][4] == 1 and
+               self.gameBoard[4][5] == 1 and self.gameBoard[5][6] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][2] == 1 and self.gameBoard[1][3] == 1 and
+               self.gameBoard[2][4] == 1 and self.gameBoard[3][5] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][3] == 1 and self.gameBoard[2][4] == 1 and
+               self.gameBoard[3][5] == 1 and self.gameBoard[4][6] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][3] == 1 and self.gameBoard[1][4] == 1 and
+               self.gameBoard[2][5] == 1 and self.gameBoard[3][6] == 1):
+            self.player1Score += 1
+
+        if (self.gameBoard[0][3] == 1 and self.gameBoard[1][2] == 1 and
+               self.gameBoard[2][1] == 1 and self.gameBoard[3][0] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][4] == 1 and self.gameBoard[1][3] == 1 and
+               self.gameBoard[2][2] == 1 and self.gameBoard[3][1] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][3] == 1 and self.gameBoard[2][2] == 1 and
+               self.gameBoard[3][1] == 1 and self.gameBoard[4][0] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][5] == 1 and self.gameBoard[1][4] == 1 and
+               self.gameBoard[2][3] == 1 and self.gameBoard[3][2] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][4] == 1 and self.gameBoard[2][3] == 1 and
+               self.gameBoard[3][2] == 1 and self.gameBoard[4][1] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][3] == 1 and self.gameBoard[3][2] == 1 and
+               self.gameBoard[4][1] == 1 and self.gameBoard[5][0] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][6] == 1 and self.gameBoard[1][5] == 1 and
+               self.gameBoard[2][4] == 1 and self.gameBoard[3][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][5] == 1 and self.gameBoard[2][4] == 1 and
+               self.gameBoard[3][3] == 1 and self.gameBoard[4][2] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][4] == 1 and self.gameBoard[3][3] == 1 and
+               self.gameBoard[4][2] == 1 and self.gameBoard[5][1] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][6] == 1 and self.gameBoard[2][5] == 1 and
+               self.gameBoard[3][4] == 1 and self.gameBoard[4][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][5] == 1 and self.gameBoard[3][4] == 1 and
+               self.gameBoard[4][3] == 1 and self.gameBoard[5][2] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][6] == 1 and self.gameBoard[3][5] == 1 and
+               self.gameBoard[4][4] == 1 and self.gameBoard[5][3] == 1):
+            self.player1Score += 1
+
+        # Check player 2
+        if (self.gameBoard[2][0] == 2 and self.gameBoard[3][1] == 2 and
+               self.gameBoard[4][2] == 2 and self.gameBoard[5][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][0] == 2 and self.gameBoard[2][1] == 2 and
+               self.gameBoard[3][2] == 2 and self.gameBoard[4][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][1] == 2 and self.gameBoard[3][2] == 2 and
+               self.gameBoard[4][3] == 2 and self.gameBoard[5][4] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][0] == 2 and self.gameBoard[1][1] == 2 and
+               self.gameBoard[2][2] == 2 and self.gameBoard[3][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][1] == 2 and self.gameBoard[2][2] == 2 and
+               self.gameBoard[3][3] == 2 and self.gameBoard[4][4] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][2] == 2 and self.gameBoard[3][3] == 2 and
+               self.gameBoard[4][4] == 2 and self.gameBoard[5][5] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][1] == 2 and self.gameBoard[1][2] == 2 and
+               self.gameBoard[2][3] == 2 and self.gameBoard[3][4] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][2] == 2 and self.gameBoard[2][3] == 2 and
+               self.gameBoard[3][4] == 2 and self.gameBoard[4][5] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][3] == 2 and self.gameBoard[3][4] == 2 and
+               self.gameBoard[4][5] == 2 and self.gameBoard[5][6] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][2] == 2 and self.gameBoard[1][3] == 2 and
+               self.gameBoard[2][4] == 2 and self.gameBoard[3][5] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][3] == 2 and self.gameBoard[2][4] == 2 and
+               self.gameBoard[3][5] == 2 and self.gameBoard[4][6] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][3] == 2 and self.gameBoard[1][4] == 2 and
+               self.gameBoard[2][5] == 2 and self.gameBoard[3][6] == 2):
+            self.player2Score += 1
+
+        if (self.gameBoard[0][3] == 2 and self.gameBoard[1][2] == 2 and
+               self.gameBoard[2][1] == 2 and self.gameBoard[3][0] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][4] == 2 and self.gameBoard[1][3] == 2 and
+               self.gameBoard[2][2] == 2 and self.gameBoard[3][1] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][3] == 2 and self.gameBoard[2][2] == 2 and
+               self.gameBoard[3][1] == 2 and self.gameBoard[4][0] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][5] == 2 and self.gameBoard[1][4] == 2 and
+               self.gameBoard[2][3] == 2 and self.gameBoard[3][2] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][4] == 2 and self.gameBoard[2][3] == 2 and
+               self.gameBoard[3][2] == 2 and self.gameBoard[4][1] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][3] == 2 and self.gameBoard[3][2] == 2 and
+               self.gameBoard[4][1] == 2 and self.gameBoard[5][0] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][6] == 2 and self.gameBoard[1][5] == 2 and
+               self.gameBoard[2][4] == 2 and self.gameBoard[3][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][5] == 2 and self.gameBoard[2][4] == 2 and
+               self.gameBoard[3][3] == 2 and self.gameBoard[4][2] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][4] == 2 and self.gameBoard[3][3] == 2 and
+               self.gameBoard[4][2] == 2 and self.gameBoard[5][1] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][6] == 2 and self.gameBoard[2][5] == 2 and
+               self.gameBoard[3][4] == 2 and self.gameBoard[4][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][5] == 2 and self.gameBoard[3][4] == 2 and
+               self.gameBoard[4][3] == 2 and self.gameBoard[5][2] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][6] == 2 and self.gameBoard[3][5] == 2 and
+               self.gameBoard[4][4] == 2 and self.gameBoard[5][3] == 2):
+            self.player2Score += 1
+        
+        if self.Computer ==1:
+            return self.player1Score-self.player2Score
+        else:
+            return self.player2Score-self.player1Score
+
---
+++ HW3/maxconnect4.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+# Written by Chris Conly based on C++
+# code provided by Dr. Vassilis Athitsos
+# Written to be Python 2.4 compatible for omega
+
+import sys
+from MaxConnect4Game import *
+
+def oneMoveGame(currentGame):
+    if currentGame.pieceCount == 42:    # Is the board full already?
+        print 'BOARD FULL\n\nGame Over!\n'
+        sys.exit(0)
+
+    currentGame.aiPlay() # Make a move (only random is implemented)
+
+    print 'Game state after move:'
+    currentGame.printGameBoard()
+
+    currentGame.countScore()
+    print('Score: Player 1 = %d, Player 2 = %d\n' % (currentGame.player1Score, currentGame.player2Score))
+
+    currentGame.printGameBoardToFile()
+    currentGame.gameFile.close()
+
+
+def interactiveGame(currentGame):
+    if currentGame.pieceCount == 42:    # Is the board full already?
+        currentGame.countScore()
+        if currentGame.Computer == 1:
+            print ('Scores:   Computer - %d      Player - %d \n' % (currentGame.player1Score,currentGame.player2Score))
+        else:
+            print ('Scores:   Computer - %d      Player - %d \n' % (currentGame.player1Score,currentGame.player2Score))
+        print 'BOARD FULL\n\nGame Over!\n'
+        sys.exit(0)
+    
+    if currentGame.currentTurn == currentGame.Computer:
+        currentGame.aiPlay() # Make a move (only random is implemented)
+        print 'Game state after move:'
+        currentGame.printGameBoard()
+    
+    else:
+        playerMove = input ('Player turn, choose a column from 1-7 :  ')
+        if currentGame.gameBoard[0][playerMove-1] !=0:
+            interactiveGame(currentGame)
+        else:
+            currentGame.playPiece(playerMove-1)
+            currentGame.pieceCount +=1
+            currentGame.currentTurn = currentGame.currentTurn%2 + 1
+            currentGame.printGameBoard()
+    interactiveGame(currentGame)
+    
+
+
+def main(argv):
+    # Make sure we have enough command-line arguments
+    if len(argv) != 5:
+        print 'Four command-line arguments are needed:'
+        print('Usage: %s interactive [input_file] [computer-next/human-next] [depth]' % argv[0])
+        print('or: %s one-move [input_file] [output_file] [depth]' % argv[0])
+        sys.exit(2)
+
+    game_mode, inFile = argv[1:3]
+
+    if not game_mode == 'interactive' and not game_mode == 'one-move':
+        print('%s is an unrecognized game mode' % game_mode)
+        sys.exit(2)
+
+    currentGame = maxConnect4Game() # Create a game
+
+    # Try to open the input file
+    try:
+        currentGame.gameFile = open(inFile, 'r')
+    except IOError:
+        sys.exit("\nError opening input file.\nCheck file name.\n")
+
+    # Read the initial game state from the file and save in a 2D list
+    file_lines = currentGame.gameFile.readlines()
+    currentGame.gameBoard = [[int(char) for char in line[0:7]] for line in file_lines[0:-1]]
+    currentGame.currentTurn = int(file_lines[-1][0])
+    currentGame.gameFile.close()
+    
+    # Get to know who is who
+    if argv[3] == 'computer-next':
+        currentGame.Computer = currentGame.currentTurn
+        currentGame.Person = currentGame.currentTurn%2 + 1
+    elif argv[3] == 'human-next':
+        currentGame.Person = currentGame.currentTurn
+        currentGame.Computer = currentGame.currentTurn%2 + 1
+    else:
+        currentGame.Computer = currentGame.currentTurn
+        currentGame.Person = currentGame.currentTurn%2 + 1
+    # Get the depth of search
+    currentGame.depthLimit = int(argv[4])
+
+    print '\nMaxConnect-4 game\n'
+    print 'Game state before move:'
+    currentGame.printGameBoard()
+
+    # Update a few game variables based on initial state and print the score
+    currentGame.checkPieceCount()
+    currentGame.countScore()
+    print('Score: Player 1 = %d, Player 2 = %d\n' % (currentGame.player1Score, currentGame.player2Score))
+
+    if game_mode == 'interactive':
+        interactiveGame(currentGame) # Be sure to pass whatever else you need from the command line
+    else: # game_mode == 'one-move'
+        # Set up the output file
+        outFile = argv[3]
+        try:
+            currentGame.gameFile = open(outFile, 'w')
+        except:
+            sys.exit('Error opening output file.')
+        oneMoveGame(currentGame) # Be sure to pass any other arguments from the command line you might need.
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
+
